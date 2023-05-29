7e24cb68f50e520b3d1c80adfd2135907bc9d2ae
Oleg Vinokurov <olegvinokurov@hotmail.com>
2023-05-29 02:42:41 -0700

    Add project files.

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TimeZoneConverterAPI.Services;
+
+namespace TimeZoneConverterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly TimeConversionService _timeConversionService;
+
+        public TimeController()
+        {
+            _timeConversionService = new TimeConversionService();
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get(string timeZone)
+        {
+            try
+            {
+                DateTime localDateTime = _timeConversionService.ConvertUtcToTimeZone(timeZone);
+                return localDateTime.ToString();
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                return NotFound($"Timezone '{timeZone}' not found.");
+            }
+            catch (InvalidTimeZoneException)
+            {
+                return BadRequest($"Timezone '{timeZone}' is invalid.");
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ Services/TimeConversionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeZoneConverterAPI.Services
+{
+    public class TimeConversionService
+    {
+        public DateTime ConvertUtcToTimeZone(string timeZone)
+        {
+            TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzi);
+        }
+    }
+}
