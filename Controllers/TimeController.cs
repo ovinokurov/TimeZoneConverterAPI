7bb2f56443cf9c5695846fc1e2f9af58201b35a2
Oleg Vinokurov <olegvinokurov@hotmail.com>
2023-05-29 03:25:56 -0700

    created read me fileadded swagger and deleted values controoler

--- Controllers/TimeController.cs
+++ Controllers/TimeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using TimeZoneConverterAPI.Services;
 
@@ -14,6 +16,24 @@ namespace TimeZoneConverterAPI.Controllers
         {
             _timeConversionService = timeConversionService;
         }
+
+        /// <summary>
+        /// Retrieves the current date and time, converted to the user-specified time zone from Coordinated Universal Time (UTC).
+        /// </summary>
+        /// <remarks>
+        /// This API endpoint requires a time zone parameter. The time zone parameter should be a valid Windows time zone identifier. Here are a few examples:
+        /// <para> - "UTC": Coordinated Universal Time </para>
+        /// <para> - "Pacific Standard Time": Pacific Time (US and Canada) </para>
+        /// <para> - "Eastern Standard Time": Eastern Time (US and Canada) </para>
+        /// <para> - "GMT Standard Time": Greenwich Mean Time (London) </para>
+        /// <para> - "India Standard Time": India Standard Time </para>
+        /// <para> - "Japan Standard Time": Japan Standard Time </para>
+        /// <para>Please make sure to use the exact time zone identifier as per the examples above.</para>
+        /// </remarks>
+        /// <param name="timeZone">The Windows time zone identifier as a string.</param>
+        /// <returns>A string representing the current date and time in the specified time zone.</returns>
+
+
 
         [HttpGet]
         public ActionResult<string> Get(string timeZone)
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# TimeZoneConverterAPI
+# TimeZone Converter API
+
+The TimeZone Converter API is a simple web service that allows you to convert the current Coordinated Universal Time (UTC) to a specified time zone.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The TimeZone Converter API provides a RESTful endpoint to retrieve the current date and time in a specific time zone. It leverages the TimeZoneConverter library to perform the time zone conversion.
+
+## Getting Started
+
+### Prerequisites
+
+- .NET Core SDK 3.1 or later
+- Visual Studio or any other code editor
+
+### Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/ovinokurov/TimeZoneConverterAPI.git
