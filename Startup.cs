5b881206aad2e7ec2a55bac204bd21375ce3ae69
Oleg Vinokurov <olegvinokurov@hotmail.com>
2023-05-29 03:25:56 -0700

    added swagger and deleted values controoler

--- Startup.cs
+++ Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TimeZoneConverterAPI.Services;
@@ -28,6 +30,12 @@ namespace TimeZoneConverterAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddScoped<TimeConversionService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Time Zone Converter API", Version = "v1" });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "TimeZoneConverterAPI.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +51,13 @@ namespace TimeZoneConverterAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Time Zone Converter API V1");
+            });
+
             app.UseMvc();
         }
     }
